b5eae1d2d4bc6277bbf8da0dd28dd3eed680d681
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-03-19 00:20:45 -0400

    implementing Ai player part 1

--- Entities/AIplayer.py
+++ Entities/AIplayer.py
@@ -1,5 +1,5 @@
 """An entity representing the game AI against which a player can play against."""
-from Entities import Symbol
+from Entities import Board, Game, Symbol
 from Entities.Player import Player
 
 
@@ -11,3 +11,38 @@ class AIPlayer(Player):
         self.symbol = symbol
         self.strategy = strategy
         self.type = "Ai"
+
+    def choose_move(self, game: Game):
+        if self.strategy.lower() == "minimax":
+            self.minimax_strategy(game)
+
+        else:
+            self.neural_net_strategy(game)
+
+
+
+    def minimax_strategy(self, game: Game):
+        # assigned values to end game conditions for the ai
+        ai_win = 1
+        ai_loss = -1
+        draw = 0
+        is_ai_turn = (game.current_player == self)
+
+
+        check_current_state = game.check_winning_combo()
+        check_board_full = game.board.check_board_full()
+        if check_current_state == self.symbol:
+            return 1
+        elif check_board_full:
+            return 0
+
+        else:
+            # implement a helper function instead
+
+
+
+
+
+
+    def neural_net_strategy(self, game: Game):
+        """Strategy involving a neural network"""
