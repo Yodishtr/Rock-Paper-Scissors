e7b79b632a38b37aa2117fa576314d4ecb868a83
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-03-22 21:15:21 -0400

    partial implementation of the interface adapter for the create game

--- App/InterfaceAdapters/CreateGameInterface/CGController.py
+++ App/InterfaceAdapters/CreateGameInterface/CGController.py
@@ -1,5 +1,6 @@
 """Controller class exclusively for the create game use case"""
 from App.UseCase_Interactors.CreateGame import CreateInteractor
+from App.UseCase_Interactors.CreateGame.CGInputData import CGInputData
 
 
 class CGController:
@@ -7,3 +8,32 @@ class CGController:
 
     def __init__(self, createInteractor: CreateInteractor) -> None:
         self.createInteractor = createInteractor
+
+    def handle_create_new(self):
+        """Executes the create new game method through the interactor and relays the output data
+        to the presenter."""
+        board_dimension_str = input("Enter the board size (eg 3,3 or 4,4): ")
+        board_dimension = tuple(int(x) for x in board_dimension_str.split(","))
+        board_rows, board_columns = board_dimension[0], board_dimension[1]
+
+        player_1_str = input("Enter first player (eg AI, Robot, X): ")
+        player_1_tup = tuple(x for x in player_1_str.split(","))
+        player1_type, player1_name, player1_symbol = (player_1_tup[0], player_1_tup[1],
+                                                      player_1_tup[2])
+
+        player_2_str = input("Enter second player (eg Human, BIGSMOKE, O): ")
+        player_2_tup = tuple(x for x in player_2_str.split(","))
+        player2_type, player2_name, player2_symbol = (player_2_tup[0], player_2_tup[1],
+                                                      player_2_tup[1])
+
+        if player1_type.lower() != "ai" and player2_type.lower() != "ai":
+            strategy = ""
+
+        else:
+            strategy = input("Enter the strategy for the AI (minimax or neural net): ")
+
+        input_data = CGInputData(board_rows, board_columns, player1_type, player2_type,
+                                 player1_name, player2_name, player1_symbol, player2_symbol,
+                                 strategy)
+
+        output_data = self.createInteractor.create_new_game(input_data)
--- App/InterfaceAdapters/CreateGameInterface/CGPresenter.py
+++ App/InterfaceAdapters/CreateGameInterface/CGPresenter.py
@@ -1,5 +1,9 @@
 """Presenter class exclusively for use by the create game use case"""
+from App.UseCase_Interactors.CreateGame import CGOutputData
 
 
 class CGPresenter:
     """Defines a presenter for the create game use case and methods to process output data"""
+
+    def __init__(self, output_data: CGOutputData) -> None:
+        """"""
